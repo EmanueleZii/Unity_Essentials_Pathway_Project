94d08227bf09936cf10b0033e11366dda9e7f451
Emanuele Zignale <zignale92@gmail.com>
2025-06-06 20:18:18 +0200

    collectible

---
+++ Assets/_Unity Essentials/Scripts/Provided Scripts/Collectible.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public float rotationspeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.Rotate(0, rotationspeed, 0); 
+    }
+}
